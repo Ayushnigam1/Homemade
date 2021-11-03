97799c1627a7fd75f9e74c76438c513cf9268542
ashwin <ashwin@pop-os.localdomain>
2021-11-03 14:04:27 +0530

    Database added

--- Api/App.js
+++ Api/App.js
@@ -3,6 +3,15 @@ const path = require('path');
 const app = express();
 const port= 5000;
 const cors = require("cors")
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://ashwin:<ashwin>@cluster0.qmfui.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+	console.log(collection)
+  client.close();
+});
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, "..", "build")));
