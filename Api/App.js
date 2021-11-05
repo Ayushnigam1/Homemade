d688df1d0e5295d5f6d837a4c2ad7f2d79d839c5
ashwin <ashwin@pop-os.localdomain>
2021-11-05 08:41:36 +0530

    db error solved

--- Api/App.js
+++ Api/App.js
@@ -4,20 +4,22 @@ const app = express();
 const port= 5000;
 const cors = require("cors")
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://ashwin:<ashwin>@cluster0.qmfui.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://mongo:mongo@cluster0.qmfui.mongodb.net/homemade?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("homemade").collection("user");
   // perform actions on the collection object
-	console.log(collection)
-  client.close();
+	collection.insertOne({
+		user : "hello"
+	}).catch(e => {
+		console.log(err)
+		console.log(e)
+	})
 });
 
 app.use(cors())
+
 app.use(express.static(path.join(__dirname, "..", "build")));
-// app.use(express.static(path.join(__dirname, "../public")));
-// console.log(path.join(__dirname, "..", "build"));
-// app.use(express.static(path.join(__dirname,"public")));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
