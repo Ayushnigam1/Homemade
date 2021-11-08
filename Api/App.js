41c5f22ff365f4c5c776923a79a97a9849b89903
ashwin <ashwin@pop-os.localdomain>
2021-11-08 09:10:52 +0530

    fixed

--- Api/App.js
+++ Api/App.js
@@ -5,19 +5,6 @@ const port= 5000;
 const cors = require("cors");
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://mongo:mongo@cluster0.qmfui.mongodb.net/homemade?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("homemade").collection("user");
-  // perform actions on the collection object
-	collection.insertOne({
-		user : "hello"
-	}).catch(e => {
-		console.log(err)
-		console.log(e)
-	})
-	console.log("successfully connected to mongo")
-  // client.close();
-});
 
 app.use(cors())
 
