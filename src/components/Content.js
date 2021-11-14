b50ab755fea2e98d1bd9fdbee1ca178abcfa68b7
Ayushnigam1 <abhay27nigam02@gmail.com>
2021-11-14 14:58:21 +0530

    front update

--- src/components/Content.js
+++ src/components/Content.js
@@ -63,6 +63,68 @@ function Content(params) {
        </div>
        </div>
         </div>
+
+        {/*category*/}
+
+        <div className="container">
+        <h2 className="subtitle">Popular category</h2>
+        </div>
+        
+        <div className="categorycontain">
+       <div className="cardcategory ">
+       <div className="cardcategoryimg">
+       </div>
+       <div className="cardcategorydata"><h5>Category 1</h5>
+       </div>
+       </div>
+       <div className="cardcategory ">
+       <div className="cardcategoryimg">
+       </div>
+       <div className="cardcategorydata"><h5>category 2</h5>
+       </div>
+       </div>
+       <div className="cardcategory ">
+       <div className="cardcategoryimg">
+       </div>
+       <div className="cardcategorydata"><h5>category 3</h5>
+       </div>
+       </div>
+       <div className="cardcategory ">
+       <div className="cardcategoryimg">
+       </div>
+       <div className="cardcategorydata"><h5>category 4</h5>
+       </div>
+       </div>
+       <div className="cardcategory ">
+       <div className="cardcategoryimg">
+       </div>
+       <div className="cardcategorydata"><h5>category 5</h5>
+       </div>
+       </div>
+       <div className="cardcategory ">
+       <div className="cardcategoryimg">
+       </div>
+       <div className="cardcategorydata"><h5>category 6</h5>
+       </div>
+       </div>
+        </div>
+
+
+        <div className="container">
+        <div className="header">
+        <div className="Bottomtext">
+        <div className="Bottomtextdata">
+        <h3 className="Bottomhead">Sharing is Caring ..</h3>
+        <p> Upload your recipes and share it with the world. </p>
+       
+       </div>
+        </div>
+        <div className="banner">
+        <div className="Bottombannerimage">
+        </div>
+        </div>
+        </div>
+        </div>
         </>
     )
 }
