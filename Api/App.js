f27657ebf950c249bf3ec388c6f71071fe31bc32
Ayushnigam1 <abhay27nigam02@gmail.com>
2021-11-14 13:27:10 +0530

    my commit changes

--- Api/App.js
+++ Api/App.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port= 5000;
+const port = 5000;
 const cors = require("cors");
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://mongo:mongo@cluster0.qmfui.mongodb.net/homemade?retryWrites=true&w=majority";
@@ -12,16 +12,16 @@ app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 app.get('/hello', (req, res) => {
-  res.send('hello world')
-  // console.log("hello ");
+    res.send('hello world')
+        // console.log("hello ");
 })
 
-app.use('/api/auth',require('./routes/auth'));
+app.use('/api/auth', require('./routes/auth'));
 
-app.listen(port,()=>{
-  console.log(`listening to the port at ${port} `);
+app.listen(port, () => {
+    console.log(`listening to the port at ${port} `);
 })
--- Api/models/User.js
+++ Api/models/User.js
@@ -1,24 +1,25 @@
 var mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-  const UserSchema = new Schema({
-    Name:{
-        type:String,
-        require:true
+const UserSchema = new Schema({
+    Name: {
+        type: String,
+        require: true
     },
-    Email:{
-        type:String,
-        require:true,
-        unique:true
+    Email: {
+        type: String,
+        require: true,
+        unique: true
     },
-    Pasward:  {
-        type:String,
-    require:true
-  },
-   Date:{
-       type:Date,
-       default:Date.now
-   }
-  });
-
-module.exports=mongoose.model('user',UserSchema);
+    Pasward: {
+        type: String,
+        require: true
+    },
+    Date: {
+        type: Date,
+        default: Date.now
+    }
+});
+const User = mongoose.model('user', UserSchema);
+User.createIndexes();
+module.exports = User;
